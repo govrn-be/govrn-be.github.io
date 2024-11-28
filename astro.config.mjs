059055aa19f8df15976f62d10f2a51b8d5ff5ec7
Ludovic Laffineur <ludovic.laffineur@gmail.com>
2024-11-28 11:33:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   //site: 'https://govrn.com',
-  site: 'https://govrn-be.github.io',
-  base: 'govrn-website-astro',
+  site: 'https://govrn-be.github.io/govrn-website-astro',
+
 });
