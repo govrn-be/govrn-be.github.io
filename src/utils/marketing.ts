9744012908694af07a6e94588efc26aca6a48a71
Ludovic Laffineur <ludovic.laffineur@gmail.com>
2024-12-04 09:53:26 +0100

    add cookie banner

---
+++ src/utils/marketing.ts
@@ -0,0 +1,9 @@
+export const initializeMarketing = () => {
+  // Initialize marketing cookies (e.g., ad tracking)
+  console.log('Marketing cookies initialized');
+};
+
+export const disableMarketing = () => {
+  // Disable marketing tracking
+  console.log('Marketing cookies disabled');
+};
