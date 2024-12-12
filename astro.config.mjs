f6efa34ad315e04276f83a5ff5a2b8c8c244baf2
Ludovic Laffineur <ludovic.laffineur@gmail.com>
2024-12-12 17:48:15 +0100

    features and news feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,6 +28,8 @@ export default defineConfig({
     '/blog/board-portal-tech-13/the-benefits-of-using-decision-logs-for-businesses-and-organizations-116': '/blog/benefits-of-decision-logs-for-businesses',
     '/blog/board-portal-tech-13/using-a-decision-log-to-track-and-document-decisions-135': '/blog/benefits-of-decision-logs-for-businesses',
     '/blog/ai-in-the-boardroom-8/ai-in-the-boardroom-a-board-grc-platform-233': '/blog/ai-boardroom-grc-platform',
+    '/board-private-chat': '/features/secure-communication',
+    '/news-feed': '/features/secure-communication'
   },
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
