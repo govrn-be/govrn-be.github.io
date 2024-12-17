fbd889bd0ba920362ac770cfa2008fbd344b8b6c
Ludovic Laffineur <ludovic.laffineur@gmail.com>
2024-12-17 16:01:06 +0100

    remove clairty

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -19,13 +19,6 @@ export const initializeAnalytics = () => {
     f.parentNode.insertBefore(j, f);
   })(window, document, "script", "dataLayer", "GTM-ML9SBCS");
 
-  // Initialize clarity 
-
-  (function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-  })(window, document, "clarity", "script", "pfeo1hi6hp");
 };
 
 
