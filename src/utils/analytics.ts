0391b74fa0a03c4bef4088c546442f2f2eeb8fd3
Ludovic Laffineur <ludovic.laffineur@gmail.com>
2024-12-17 14:33:56 +0100

    add clarity

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -19,21 +19,16 @@ export const initializeAnalytics = () => {
     f.parentNode.insertBefore(j, f);
   })(window, document, "script", "dataLayer", "GTM-ML9SBCS");
 
-  // Initialize Hotjar
-  (function (h, o, t, j, a, r) {
-    h.hj =
-      h.hj ||
-      function () {
-        (h.hj.q = h.hj.q || []).push(arguments);
-      };
-    h._hjSettings = { hjid: 3557875, hjsv: 6 };
-    a = o.getElementsByTagName("head")[0];
-    r = o.createElement("script");
-    r.async = 1;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+  // Initialize clarity 
+
+  (function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+  })(window, document, "clarity", "script", "pfeo1hi6hp");
 };
+
+
 
 export const disableAnalytics = () => {
   // Disable analytics tracking
